456822b39cdfab13a42c26c00da9193b9100d958
Kirti Swaraj <Sai@KirtiSwaraj>
2020-10-21 16:24:52 +0530

    [kirti]Add . find max among three float

--- GenericsTestMaximum/FindMaximum.cs
+++ GenericsTestMaximum/FindMaximum.cs
@@ -31,5 +31,24 @@ namespace GenericsTestMaximum
             else
                 return thirdValue;
         }
+        /// <summary>
+        /// UC 2 : Finds Maximum float value among three.
+        /// </summary>
+        /// <param name="firstValue">The first value.</param>
+        /// <param name="secondValue">The second value.</param>
+        /// <param name="thirdValue">The third value.</param>
+        /// <returns></returns>
+        public static double MaxFloatAmongThree(double firstValue, double secondValue, double thirdValue)
+        {
+            Console.WriteLine("Maximum number among {0}, {1} and {2}: ", firstValue, secondValue, thirdValue);
+            /// Checks if firstValue is greater than both secondValue and thirdValue
+            if (firstValue.CompareTo(secondValue) > 0 && firstValue.CompareTo(thirdValue) > 0)
+                return firstValue;
+            /// Checks if secondValue is greater than both firstValue and thirdValue
+            else if (secondValue.CompareTo(firstValue) > 0 && secondValue.CompareTo(thirdValue) > 0)
+                return secondValue;
+            else
+                return thirdValue;
+        }
     }
 }
--- MSTestFindMax/UnitTest1.cs
+++ MSTestFindMax/UnitTest1.cs
@@ -48,5 +48,46 @@ namespace MSTestFindMax
             //Assert
             Assert.AreEqual(expected, actual);
         }
+        /// <summary>
+        /// UC 2.1 : Given the maximum float at first position returns the same number.
+        /// </summary>
+        [TestMethod]
+        public void GivenMaxFloatAtFirstPos_ReturnTheSameNumber()
+        {
+            //Arrange
+            double expected = 10.55;
+            //Act
+            double actual = FindMaximum.MaxFloatAmongThree(10.55, 09, 10.2);
+            //Assert
+            Assert.AreEqual(expected, actual);
+        }
+
+        /// <summary>
+        /// UC 2.2 : Given the maximum float at second position returns the same number.
+        /// </summary>
+        [TestMethod]
+        public void GivenMaxFloatAtSecondPos_ReturnTheSameNumber()
+        {
+            //Arrange
+            double expected = 0.45;
+            //Act
+            double actual = FindMaximum.MaxFloatAmongThree(0.25, 0.45, 0.2);
+            //Assert
+            Assert.AreEqual(expected, actual);
+        }
+
+        /// <summary>
+        /// UC 2.3 : Given the maximum float at third position returns the same number.
+        /// </summary>
+        [TestMethod]
+        public void GivenMaxFloatAtThirdPos_ReturnTheSameNumber()
+        {
+            //Arrange
+            double expected = 6.6;
+            //Act
+            double actual = FindMaximum.MaxFloatAmongThree(1.256, 3.85, 6.6);
+            //Assert
+            Assert.AreEqual(expected, actual);
+        }
     }
 }
