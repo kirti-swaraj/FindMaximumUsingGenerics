61c3c60f44434888da485eaff6a06d3a3b1e0e77
Kirti Swaraj <Sai@KirtiSwaraj>
2020-10-22 06:58:22 +0530

    [kirti] Add . Find the maximum among any number of data

--- GenericsTestMaximum/GenericsMaximum.cs
+++ GenericsTestMaximum/GenericsMaximum.cs
@@ -5,42 +5,63 @@ using System.Text;
 namespace GenericsTestMaximum
 {
     public class GenericMaximum<T> where T : IComparable
-    {
-        T firstValue, secondValue, thirdValue;
-        public GenericMaximum(T firstValue, T secondValue, T thirdValue)
-        {
-            this.firstValue = firstValue;
-            this.secondValue = secondValue;
-            this.thirdValue = thirdValue;
-        }
+    { 
+       public T[] value;
 
-        /// <summary>
-        /// UC 3 : Refactor 2: Finds the Maximum value among three using generic.
-        /// </summary>
-        /// <param name="firstValue">The first value.</param>
-        /// <param name="secondValue">The second value.</param>
-        /// <param name="thirdValue">The third value.</param>
-        /// <returns></returns>
-        private static T MaximumValueAmongThree(T firstValue, T secondValue, T thirdValue)
-        {
-            Console.WriteLine("Maximum value among {0}, {1} and {2}: ", firstValue, secondValue, thirdValue);
-            /// Checks if firstValue is great then both secondValue and thirdValue
-            if (firstValue.CompareTo(secondValue) >= 0 && firstValue.CompareTo(thirdValue) >= 0)
-                return firstValue;
-            /// Checks if secondValue is great then both firstValue and thirdValue
-            else if (secondValue.CompareTo(firstValue) >= 0 && secondValue.CompareTo(thirdValue) >= 0)
-                return secondValue;
-            else
-                return thirdValue;
-        }
-        /// <summary>
-        /// UC 3 : Refactor 2 : Finds the maximum using generic
-        /// </summary>
-        /// <returns></returns>
-        public T MaxMethod()
-        {
-            T maxValue = MaximumValueAmongThree(this.firstValue, this.secondValue, this.thirdValue);
-            return maxValue;
+            public GenericMaximum(T[] value)
+            {
+                this.value = value;
+            }
+
+            /// <summary>
+            /// UC 4 : Sorts the specified values.
+            /// </summary>
+            /// <param name="values">The values.</param>
+            /// <returns></returns>
+            public T[] Sort(T[] values)
+            {
+                Array.Sort(values);
+                return values;
+            }
+
+            /// <summary>
+            /// UC 4 : Finds Maximum value.
+            /// </summary>
+            /// <param name="values">The values.</param>
+            /// <returns></returns>
+            private T MaxValue(params T[] values)
+            {
+                var sortedValues = Sort(values);
+                return sortedValues[^1];
+            }
+
+            /// <summary>
+            /// UC 3 : Refactor 2: Finds the Maximum value among three using generic.
+            /// </summary>
+            /// <param name="firstValue">The first value.</param>
+            /// <param name="secondValue">The second value.</param>
+            /// <param name="thirdValue">The third value.</param>
+            /// <returns></returns>
+            private static T MaximumValueAmongThree(T firstValue, T secondValue, T thirdValue)
+            {
+                Console.WriteLine("Maximum value among {0}, {1} and {2}: ", firstValue, secondValue, thirdValue);
+                /// Checks if firstValue is great then both secondValue and thirdValue
+                if (firstValue.CompareTo(secondValue) >= 0 && firstValue.CompareTo(thirdValue) >= 0)
+                    return firstValue;
+                /// Checks if secondValue is great then both firstValue and thirdValue
+                else if (secondValue.CompareTo(firstValue) >= 0 && secondValue.CompareTo(thirdValue) >= 0)
+                    return secondValue;
+                else
+                    return thirdValue;
+            }
+            /// <summary>
+            /// UC 3 : Refactor 2 : Finds the maximum using generic
+            /// </summary>
+            /// <returns></returns>
+            public T MaxMethod()
+            {
+                var maxValue = MaxValue(this.value);
+                return maxValue;
+            }
         }
     }
-}
--- GenericsTestMaximum/Program.cs
+++ GenericsTestMaximum/Program.cs
@@ -12,25 +12,35 @@ namespace GenericsTestMaximum
     {
         static void Main(string[] args)
         {
-            ///UC 1 : Finds max among the given three data
-            Console.WriteLine(FindMaximum.MaxIntegerAmongThree(89, 12, 50));
-            /// UC 2 : Finds the max float value among three
-            Console.WriteLine(FindMaximum.MaxFloatAmongThree(6.986, 6.64, 6.948));
-            /// UC 3 : Finds the max string value among three
-            Console.WriteLine(FindMaximum.MaxStringAmongThree("Apple", "Banana", "Peach"));
+            //UC 1 : Finds max among the given three data
+            //Console.WriteLine(FindMaximum.MaxIntegerAmongThree(89, 12, 50));
+            // UC 2 : Finds the max float value among three
+            //Console.WriteLine(FindMaximum.MaxFloatAmongThree(6.986, 6.64, 6.948));
+            // UC 3 : Finds the max string value among three
+            //Console.WriteLine(FindMaximum.MaxStringAmongThree("Apple", "Banana", "Peach"));
 
-            /// UC 3: Refactor 1
-            Console.WriteLine(FindMaximum.MaxValueAmongThreeRefactor1<int>(13, 47, 66));
-            Console.WriteLine(FindMaximum.MaxValueAmongThreeRefactor1<double>(19.85, 178.856, 14.59));
-            Console.WriteLine(FindMaximum.MaxValueAmongThreeRefactor1<string>("Spring", "Sports", "Badminton"));
+            // UC 3: Refactor 1
+            //Console.WriteLine(FindMaximum.MaxValueAmongThreeRefactor1<int>(13, 47, 66));
+            //Console.WriteLine(FindMaximum.MaxValueAmongThreeRefactor1<double>(19.85, 178.856, 14.59));
+            //Console.WriteLine(FindMaximum.MaxValueAmongThreeRefactor1<string>("Spring", "Sports", "Badminton"));
 
-            /// UC 3 : Refactor 2
-            GenericMaximum<int> maxInt = new GenericMaximum<int>(58, 96, 102);
-            GenericMaximum<double> maxFloat = new GenericMaximum<double>(27.59, 35.98, 12.877);
-            GenericMaximum<string> maxString = new GenericMaximum<string>("Kirti", "Pakhi", "Golu");
-            Console.WriteLine(maxInt.MaxMethod());
-            Console.WriteLine(maxFloat.MaxMethod());
-            Console.WriteLine(maxString.MaxMethod());
+            // UC 3 : Refactor 2
+            //GenericMaximum<int> maxInt = new GenericMaximum<int>(58, 96, 102);
+            //GenericMaximum<double> maxFloat = new GenericMaximum<double>(27.59, 35.98, 12.877);
+            //GenericMaximum<string> maxString = new GenericMaximum<string>("Kirti", "Pakhi", "Golu");
+            //Console.WriteLine(maxInt.MaxMethod());
+            //Console.WriteLine(maxFloat.MaxMethod());
+            //Console.WriteLine(maxString.MaxMethod());
+
+            /// UC 4: Find maxm among any value of int
+            int[] arr1 = { 12, 45, 67, 234, 67 };
+            GenericMaximum<int> generic = new GenericMaximum<int>(arr1);
+            Console.WriteLine(generic.MaxMethod());
+
+            /// UC 4: Find maxm among any value of string
+            string[] arr2 = { "Ram", "Shyam", "Geeta", "Zebra" };
+            GenericMaximum<string> gen = new GenericMaximum<string>(arr2);
+            Console.WriteLine(gen.MaxMethod());
         }
     }
 }
